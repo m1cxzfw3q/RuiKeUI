4452b864919dd9d20909599bc3cb615729c79d7e
m1cxzfw3q <mcxzfwq@outlook.com>
2026-09-26 14:21:41 +0800

    update

--- src/ruikeUI/RKMain.java
+++ src/ruikeUI/RKMain.java
@@ -16,12 +16,6 @@ public class RKMain extends Mod {
     public static Music boss3, boss4;
 
     public RKMain() {
-        // 修改原版音乐
-        boss3 = Vars.tree.loadMusic("boss3");
-        boss4 = Vars.tree.loadMusic("boss4");
-
-        Vars.control.sound.bossMusic.clear().addAll(Musics.boss1, Musics.boss2, boss3, boss4);
-
         Events.on(EventType.ClientLoadEvent.class, e -> {
             // 替换鼠标贴图
             Vars.ui.drillCursor = Core.graphics.newCursor(
@@ -57,6 +51,12 @@ public class RKMain extends Mod {
                             new Pixmap(Vars.tree.get("cursors/ibeam.png")), 1, Color.valueOf("3f3f3fff"), 1
                     )
             );
+
+            // 修改原版音乐
+            boss3 = Vars.tree.loadMusic("boss3");
+            boss4 = Vars.tree.loadMusic("boss4");
+
+            Vars.control.sound.bossMusic.clear().addAll(Musics.boss1, Musics.boss2, boss3, boss4);
         });
     }
 }
